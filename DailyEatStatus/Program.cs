d5e4fc5236804ecc7fc87176521811c2e3c8e181
LENOVO <LENOVO@DESKTOP-0DQDIGG>
2021-05-19 20:48:37 +0530

    Revert "message"
    This reverts commit 7cb3e55532dd0742132485a87ce6f0488b32c4d1.

--- DailyEatStatus/Program.cs
+++ DailyEatStatus/Program.cs
@@ -11,10 +11,13 @@ namespace DailyEatStatus
             Console.WriteLine("Hello Mirha!");
             Console.WriteLine("Hello Mirha11!");
             Console.WriteLine("Hello Mirha12!");
+<<<<<<< HEAD
             Console.WriteLine("Hello Mirha222!");
             Console.WriteLine("Hello Mirha223!");
 =======
 >>>>>>> parent of 31680c6 (chnage 2nd)
+=======
+>>>>>>> parent of 7cb3e55 (message)
             DailyEat.foodfunc();
         }
     }
