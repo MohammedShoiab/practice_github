0502937ca9d35ef7ac081d3604887f3cf9977451
LENOVO <LENOVO@DESKTOP-0DQDIGG>
2021-05-19 16:36:03 +0530

    first commit

---
+++ DailyEatStatus/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DailyEatStatus
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            DailyEat.foodfunc();
+        }
+    }
+    public  class DailyEat
+    {
+        public static string food;
+        public static DateTime time;
+
+        public static void foodfunc()
+        {
+            food = "rice";
+            Console.WriteLine("food is awsome {0}", food);
+            Console.WriteLine("time is " + time.TimeOfDay);
+        }
+    }
+}
